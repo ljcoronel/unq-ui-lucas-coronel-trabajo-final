b5434791e0734ce1f5b4219b01635ae0ba02f940
Lucas Josué Coronel <142062495+ljcoronel@users.noreply.github.com>
2025-07-03 19:44:34 -0300

    Fix manejo de errores

--- Web/src/services/Api.js
+++ Web/src/services/Api.js
@@ -3,16 +3,26 @@ import { API_URL } from "../constants.js";
 
 const baseURL = API_URL;
 
+const errorHandler = (error, code, message) => {if (error.response.status === code) throw Error(message)}
+
 const getDifficulties = () => axios.get(`/api/difficulties`, { baseURL })
-    .then(response => response.data);
+    .then(response => response.data)
+    .catch(error => errorHandler(error, 500, "Error del servidor"));
 
 const getGameSession = (id) => axios.get(`/api/difficulties/${id}`, { baseURL })
     .then(response => response.data)
-    .catch(error => {if (error.response.status === 404) throw Error(error.response.data.message)});
+    .catch(error => {
+        errorHandler(error, 500, "Error del servidor");
+        errorHandler(error, 404, "No se encontro la dificultad");
+    });
 
 const checkWord = (sessionId, word) => axios.post(`/api/checkWord`, { sessionId, word }, { baseURL })
     .then(response => response.data)
-    .catch(error => {if (error.response.status === 400 || error.response.status === 404) throw Error(error.response.data.message)});
+    .catch(error => {
+        errorHandler(error, 500, "Error del servidor");
+        errorHandler(error, 404, "No se encontro la sesion");
+        errorHandler(error, 400, "Palabra incorrecta");
+    });
 
 export default {
     getDifficulties,
