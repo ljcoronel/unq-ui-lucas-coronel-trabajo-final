6ec0b9bc3ceaf64aa37bf2bbe2786ac189555335
Lucas Josué Coronel <142062495+ljcoronel@users.noreply.github.com>
2025-07-08 20:54:29 -0300

    Se agrego la funcion newGame en el contexto de Wordle Se agrego logica para manejar las letras y el borrado en handleKeyup del contexto de Wordle

--- Web/src/hooks/useWordleContext.jsx
+++ Web/src/hooks/useWordleContext.jsx
@@ -1,6 +1,7 @@
 import {createContext, useContext, useState} from "react";
 
 const WordleContext = createContext({
+    session: {sessionId: "", difficulty: {}, wordLenght: 0},
     turn: 0,
     currentGuess: "",
     guesses: [],
@@ -9,6 +10,7 @@ const WordleContext = createContext({
 });
 
 export const WordleProvider = ({ children }) => {
+    const [session, setSession] = useState({sessionId: "", difficulty: {}, wordLenght: 0});
     const [turn, setTurn] = useState(0); //turno del 0 al 5
     const [currentGuess, setCurrentGuess] = useState(""); //la palabra
     const [guesses, setGuesses] = useState([]); //la estructura de la palabra por api
@@ -19,10 +21,29 @@ export const WordleProvider = ({ children }) => {
 
     const addNewGuess = () => {};
 
-    const handleKeyup = () => {};
+    const handleKeyup = ({ key }) => {
+        if (key === "Backspace") {
+            setCurrentGuess((prev) => prev.slice(0, -1));
+            return;
+        }
+        if (/^[A-Za-zÑñ]$/.test(key)) {
+            if (currentGuess.length < session.wordLenght) {
+                setCurrentGuess(prev => prev + key);
+            }
+        }
+    };
+
+    const newGame = (session) => {
+        setSession(session);
+        setTurn(0);
+        setCurrentGuess("");
+        setGuesses([]);
+        setHistory([]);
+        setIsCorrect(false);
+    };
 
     return (
-        <WordleContext.Provider value={{ turn, currentGuess, guesses, isCorrect, handleKeyup }}>
+        <WordleContext.Provider value={{ session, turn, currentGuess, guesses, isCorrect, handleKeyup, newGame }}>
             {children}
         </WordleContext.Provider>
     );
