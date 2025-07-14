15e818f6ad0b4eba833c9212e78cdf00c90a4c22
Lucas Josué Coronel <142062495+ljcoronel@users.noreply.github.com>
2025-07-14 19:25:09 -0300

    Se agrego al componente GameError una modal si sale un error relacionado al juego

--- Web/src/components/GameError.jsx
+++ Web/src/components/GameError.jsx
@@ -12,7 +12,20 @@ function GameOver() {
 
     return (
         <div>
-            {error && showError && <div>{error.message}</div>}
+            {error && showError && (
+                <div className="modal show fade d-block" tabIndex="-1" role="dialog">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header bg-warning text-white">
+                                <h5 className="modal-title">Error</h5>
+                            </div>
+                            <div className="modal-body">
+                                <p>{error.message}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
