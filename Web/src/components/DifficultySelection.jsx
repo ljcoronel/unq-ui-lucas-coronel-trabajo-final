e3be4b6b8f771fd085d0240e05165de33c65f397
Lucas Josué Coronel <142062495+ljcoronel@users.noreply.github.com>
2025-07-14 19:27:01 -0300

    Se cambio en el componente DifficultySelection el cargando por un spinner y se agrego el manejo de errores genericos

--- Web/src/components/DifficultySelection.jsx
+++ Web/src/components/DifficultySelection.jsx
@@ -1,16 +1,18 @@
 import {useWordleContext} from "../hooks/useWordleContext.jsx";
 import Api from "../services/Api.js";
 import {useEffect, useState} from "react";
+import GenericError from "./GenericError.jsx";
 
 function DifficultySelection({ handleDifficulty }) {
     const { loading } = useWordleContext();
     const [difficulties, setDifficulties] = useState([]);
     const [screenLoading, setScreenLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         Api.getDifficulties()
             .then(response => setDifficulties(response))
-            .catch(e => console.log(e.message))
+            .catch(e => setError(e))
             .finally(() => setScreenLoading(false));
     }, []);
 
@@ -21,7 +23,9 @@ function DifficultySelection({ handleDifficulty }) {
 
     if (screenLoading) {
         return (
-            <div>Cargando...</div>
+            <div className="spinner-border text-primary" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </div>
         );
     }
 
@@ -35,6 +39,7 @@ function DifficultySelection({ handleDifficulty }) {
                     </button>
                 ))}
             </div>
+            <GenericError error={error} />
         </div>
     );
 }
