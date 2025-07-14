656916b8df27259bd4166a8b795b5271f7629fb7
Lucas Josué Coronel <142062495+ljcoronel@users.noreply.github.com>
2025-07-14 19:23:39 -0300

    Se agrego al componente GameOver una modal si sale un error relacionado al juego

--- Web/src/components/GameOver.jsx
+++ Web/src/components/GameOver.jsx
@@ -7,18 +7,39 @@ function GameOver({ handleDifficulty }) {
     return (
         <div>
             {isCorrect && (
-                <div>
-                    <div>¡Ganaste! 🏆</div>
-                    <div>Adivinaste la palabra en {turn} {turn === 1 ? "intento" : "intentos"}</div>
+                <div className="modal show fade d-block" tabIndex="-1" role="dialog">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header bg-success text-white">
+                                <h5 className="modal-title">¡Ganaste! 🏆</h5>
+                            </div>
+                            <div className="modal-body">
+                                <p>Adivinaste la palabra en {turn} {turn === 1 ? "intento" : "intentos"}</p>
+                            </div>
+                            <div className="modal-footer">
+                                <RestartGame onClick={() => handleDifficulty(session.difficulty.id)} />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             )}
-            {!isCorrect && turn === 6 && (
-                <div>
-                    <div>¡Perdiste!</div>
-                    <div>Usaste los 6 intentos</div>
+            {!isCorrect && turn > 5 && (
+                <div className="modal show fade d-block" tabIndex="-1" role="dialog">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header bg-danger text-white">
+                                <h5 className="modal-title">¡Perdiste!</h5>
+                            </div>
+                            <div className="modal-body">
+                                <p>Usaste los 6 intentos</p>
+                            </div>
+                            <div className="modal-footer">
+                                <RestartGame onClick={() => handleDifficulty(session.difficulty.id)} />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             )}
-            {(isCorrect || turn === 6) && <RestartGame onClick={() => handleDifficulty(session.difficulty.id)} />}
         </div>
     );
 }
