a100acea072143c155dc50adb19064b8e2174287
Lucas Josué Coronel <142062495+ljcoronel@users.noreply.github.com>
2025-07-16 19:00:26 -0300

    Fix teclado virtual

--- Web/src/components/Keypad.jsx
+++ Web/src/components/Keypad.jsx
@@ -4,13 +4,18 @@ import keys from "../constants/keys.js";
 function Keypad() {
     const { usedKeys, loading, handleKeyup } = useWordleContext();
 
+    const handleClick = (e, key) => {
+        e.currentTarget.blur();
+        handleKeyup(key);
+    };
+
     return (
         <div className="keypad">
             {keys.map((l) => {
                 const letter = l.key.toLowerCase();
                 const solution = usedKeys[letter];
                 return (
-                    <button key={l.key} className={solution} onClick={() => handleKeyup({ key: l.key })} disabled={loading}>
+                    <button key={l.key} className={solution} onClick={(e) => handleClick(e, { key: l.key })} disabled={loading}>
                         {(l.key === "Enter") ? "⏎" : (l.key === "Backspace") ? "⌫" : l.key}
                     </button>
                 );
